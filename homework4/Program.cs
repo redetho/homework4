f8ba16eb991d7a9d2e6f6b334f3a46ce9473ca8a
Thomas Redekop <redetho@gmail.com>
2021-09-15 14:37:52 -0300

    Adicionar arquivos de projeto.

---
+++ homework4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization; // for lots of globalization types
+
+namespace homework4
+{
+
+    class Program
+    {
+
+        static double Pi = 3.14;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double circ = Circuferencia(raio);
+            double volume = Volume(raio);
+            Console.WriteLine("Circuferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
+        }
+        static double Circuferencia(double r)
+        {
+            return 2.0 * Pi * r;
+        }
+        static double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * r * r * r;
+        }
+    }
+
+    
+}
