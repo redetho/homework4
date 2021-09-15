dc49e76252cccf46b27b4450dabf23572c980c90
Thomas Redekop <redetho@gmail.com>
2021-09-15 15:03:05 -0300

    part2

--- homework4/Program.cs
+++ homework4/Program.cs
@@ -3,35 +3,26 @@ using System.Globalization; // for lots of globalization types
 
 namespace homework4
 {
-
     class Program
     {
-        //declare Pi value
-        static double Pi = 3.14;
-        static void Main(string[] args)
-        {
-            //ask for value and calculate circumference, as well as volume and radius.
-            Console.WriteLine("Entre com valor do raio: ");
-            //sends values to other functions for calculations
-            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            double circ = Circuferencia(raio);
-            double volume = Volume(raio);
-            //presents results
-            Console.WriteLine("Circuferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("Valor de PI: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
-        }
-        static double Circuferencia(double r)
-        {
-            //measures circumference
-            return 2.0 * Pi * r;
-        }
-        static double Volume(double r)
-        {
-            //measures volume
-            return 4.0 / 3.0 * Pi * r * r * r;
-        }
+        //creates a new calculadora class
+        calculadora calc = new calculadora();
+        //asks for radius value, sends info to calculadora class
+        Console.WriteLine("Entre o valor do raio: ");
+        double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double circ = calc.Circuferencia(raio);
+        double volume = calc.Volume(raio);
+        //show results
+        Console.WriteLine("Circuferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+        Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+        Console.WriteLine("Valor de PI: " + calc.Pi.ToString("F2", CultureInfo.InvariantCulture));
+    }
+    class calculadora
+    {
+        //math formulas
+        public double Pi = 3.14;
+        public double Circunferencia(double r) { return 2.0 * Pi * r; }
+        public double Volume(double r) { return 4.0 / 3.0 * Pi * r * r * r; }
     }
 
-    
 }
